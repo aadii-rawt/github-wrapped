d1e3d03f7ffbdbc6622b4754cc4095a45a9ff3b2
aadii-rawt <rawatadii060@gmail.com>
2025-06-30 17:34:57 +0530

    stats

--- client/src/components/Slideshow.jsx
+++ client/src/components/Slideshow.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import { MdKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
-
+import { MdFileDownload, MdKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { IoIosShareAlt } from "react-icons/io";
 const slides = [
     {
         id: 1,
@@ -113,15 +113,57 @@ const SlideShow = () => {
                     className="absolute inset-0 flex items-center justify-center text-white transition-opacity duration-700 ease-in-out mt-5"
                 >
                     <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+                       
+                       {/* profile and */}
                         <div className="p-3 ">
-                            <img src="https://i.pinimg.com/originals/3a/db/54/3adb54574e061563341609f4b69c865f.gif" alt="" className="rounded"/>
+                            <img src="https://i.pinimg.com/originals/3a/db/54/3adb54574e061563341609f4b69c865f.gif" alt="" className="rounded" />
 
                             <div className="flex items-center justify-center -mt-8 flex-col">
-
-                            <img src="https://i.pinimg.com/736x/d9/17/c9/d917c942d6d48c5a763e35e59eb92a04.jpg" alt="" className="w-16 h-16 rounded-full border-2" />
-                            <h1 className="mt-2 font-semibold">aadii-rawt</h1>
-
+                                <img src="https://i.pinimg.com/736x/d9/17/c9/d917c942d6d48c5a763e35e59eb92a04.jpg" alt="" className="w-16 h-16 rounded-full border-2" />
+                                <h1 className="mt-2 font-semibold">aadii-rawt</h1>
                             </div>
+                        </div>
+
+                        {/* github stats */}
+
+                        <div className="grid grid-cols-2 p-3 justify-between gap-4">
+                            <div className="bg-pink-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Character</h1>
+                                <h1 className="text-pink-500 font-semibold text-lg">Itachi Uchiha</h1>
+                            </div>
+                            <div className="bg-green-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Longest Streak</h1>
+                                <h1 className="text-green-500 font-semibold text-lg">99 days</h1>
+                            </div>
+                            <div className="bg-pink-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Character</h1>
+                                <h1 className="text-pink-500 font-semibold text-lg">Itachi Uchiha</h1>
+                            </div>
+                            <div className="bg-green-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Longest Streak</h1>
+                                <h1 className="text-green-500 font-semibold text-lg">99 days</h1>
+                            </div>
+                            <div className="bg-pink-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Character</h1>
+                                <h1 className="text-pink-500 font-semibold text-lg">Itachi Uchiha</h1>
+                            </div>
+                            <div className="bg-green-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Longest Streak</h1>
+                                <h1 className="text-green-500 font-semibold text-lg">99 days</h1>
+                            </div>
+                            <div className="bg-pink-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Character</h1>
+                                <h1 className="text-pink-500 font-semibold text-lg">Itachi Uchiha</h1>
+                            </div>
+                            <div className="bg-green-300/20 p-2 rounded-md"> 
+                                <h1 className="text-[13px]">Longest Streak</h1>
+                                <h1 className="text-green-500 font-semibold text-lg">99 days</h1>
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <button><IoIosShareAlt /></button>
+                            <button><MdFileDownload /></button>
                         </div>
                     </div>
                 </div>
@@ -137,7 +179,7 @@ const SlideShow = () => {
                     onClick={goPrev}
                     disabled={currentIndex === 0}
                     className={`"bg-white/20 bg-white/10 text-white p-3 rounded-full disabled:opacity-30 ${currentIndex === 0 ? "cursor-not-allowed" : "cursor-pointer"}`}
-                >
+                >                                                                         
                     <MdOutlineKeyboardArrowLeft Left size={20} />
                 </button>
                 <button
