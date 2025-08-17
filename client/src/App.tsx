5fa05f989ad055c57d540f15cf12b1e73e840332
aadii-rawt <rawatadii060@gmail.com>
2025-08-17 20:27:48 +0530

    ui fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Hero from './components/Hero'
 import SlideShow from './components/Slideshow'
 import { GlobalProvider } from './context/GloabalContext'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <div className='bg-black'>
       <div className='max-w-[1400px] mx-auto bg-black relative'>
-
         <GlobalProvider>
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={<Hero />} />
+              <Route path='/' element={<Home />} />
               <Route path='/stats' element={<SlideShow />} />
             </Routes>
           </BrowserRouter>
---
+++ client/src/pages/Share.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Share: React.FC = () => {
+    return (
+        <div className='mx-w-xl '>
+
+        </div>
+    )
+}
+
+export default Share
